f663f3a3c92a0c3325799351f9e1ae94c5f8a11a
Manolis Skouris <skourismanolis@gmail.com>
2021-12-09 17:59:16 +0200

    fix size type warn

--- src/BK_Entry.cpp
+++ src/BK_Entry.cpp
@@ -16,7 +16,7 @@ Entry *BK_Entry::get(bud::string q)
 		else
 		{
 			lookList.pop_back();
-			for (int i = 1; i <= edge->_edges.size(); i++)
+			for (bud::size_type i = 1; i <= edge->_edges.size(); i++)
 			{
 				lookList.push_back(edge->_edges.at(i));
 			}
