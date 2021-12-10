6eb5684d90434f2e92834576e9342945f87e5e8d
Manolis Skouris <skourismanolis@gmail.com>
2021-12-10 14:24:54 +0200

    add bk entry search

--- src/BK_Entry.cpp
+++ src/BK_Entry.cpp
@@ -16,11 +16,51 @@ Entry *BK_Entry::get(bud::string q)
 		else
 		{
 			lookList.pop_back();
-			for (auto & newEdge : edge->_edges)
+			for (auto &newEdge : edge->_edges)
 			{
 				lookList.push_back(newEdge);
 			}
 		}
 	}
 	return NULL;
+}
+
+bud::vector<bud::pair<Entry *, int>> BK_Entry::search(bud::string query, int maxDistance) const
+{
+	// handle null root
+	if (root == NULL)
+		return bud::vector<bud::pair<Entry *, int>>();
+	else
+		return recSearch(root, query, maxDistance);
+}
+
+bud::vector<bud::pair<Entry *, int>> BK_Entry::recSearch(Node *parent, bud::string query, int maxDistance) const
+{
+	bud::vector<bud::pair<Entry *, int>> results;
+
+	Entry temp(query, bud::unordered_set<Query *>());
+
+	// if the parent node fits our criteria, add to results and explore it's children
+	int distance = distanceFunction(parent->content, &temp, 0);
+	if (distance <= maxDistance && !parent->deleted)
+		results.push_back(bud::pair(parent->content, distance));
+
+	// iterate over parent's children, explore only those with
+	// |child->parentDistance - maxDistance| <= maxDistance <= |child->parentDistance + maxDistance|
+	for (const auto &edge : parent->_edges)
+	{
+		// if the child's parentDistance fits our criteria, call recSearch
+		if (abs(edge->parentDistance - maxDistance) <= maxDistance ||
+			maxDistance <= abs(edge->parentDistance + maxDistance))
+		{
+
+			bud::vector<bud::pair<Entry *, int>> temp_results = recSearch(edge, query, maxDistance);
+			// add it's results to ours
+			for (const auto &result : temp_results)
+			{
+				results.push_back(result);
+			}
+		}
+	}
+	return results;
 }
--- tests/bk_entry_tests.cpp
+++ tests/bk_entry_tests.cpp
@@ -2,6 +2,7 @@
 
 #include "../src/BK_Entry.h"
 #include "../src/appMatching/editDistance.h"
+#include "../src/unique_ptr.h"
 
 static int distance(Entry *a, Entry *b, unsigned int max_t)
 {
@@ -25,6 +26,57 @@ TEST_CASE("BK Entry get", "[bk_entry_get]")
 	REQUIRE(tree.get("heyy") == &b);
 	REQUIRE(tree.get("hola") == &c);
 
-
 	REQUIRE(tree.get("this does not exist in the BK") == NULL);
+}
+
+TEST_CASE("BK Entry search", "[bk_entry_search]")
+{
+	bud::string words[7] = {"hell", "help", "fall", "felt", "fell", "small", "melt"};
+	bud::vector<Entry *> entries;
+	BK_Entry tree(&distance);
+
+	//create entries and add to tree
+	for (int i = 0; i < 7; i++)
+	{
+		Entry *entry = new Entry(words[i], bud::unordered_set<Query *>());
+		entries.push_back(entry);
+		tree.insert(entry);
+	}
+
+	bud::vector<bud::pair<Entry *, int>> results = tree.search("hell", 0);
+
+	REQUIRE(results.size() == 1);
+	REQUIRE(results.at(0).first == entries.at(0));
+
+	results = tree.search("hell", 1);
+
+	REQUIRE(results.size() == 3);
+
+	REQUIRE(results.at(0).first == entries.at(0)); //hell
+	REQUIRE(results.at(0).second == 0);
+	REQUIRE(results.at(1).first == entries.at(1)); //help
+	REQUIRE(results.at(1).second == 1);
+	REQUIRE(results.at(2).first == entries.at(4)); //fell
+	REQUIRE(results.at(2).second == 1);
+
+	results = tree.search("hell", 2);
+
+	REQUIRE(results.size() == 6);
+	REQUIRE(results.at(0).first == entries.at(0)); //hell
+	REQUIRE(results.at(0).second == 0);
+	REQUIRE(results.at(1).first == entries.at(1)); //help
+	REQUIRE(results.at(1).second == 1);
+	REQUIRE(results.at(2).first == entries.at(4)); //fell
+	REQUIRE(results.at(2).second == 1);
+	REQUIRE(results.at(3).first == entries.at(2)); //fall
+	REQUIRE(results.at(3).second == 2);
+	REQUIRE(results.at(4).first == entries.at(3)); //felt
+	REQUIRE(results.at(4).second == 2);
+	REQUIRE(results.at(5).first == entries.at(6)); //melt
+	REQUIRE(results.at(5).second == 2);
+
+	for (auto &entry : entries)
+	{
+		delete entry;
+	}
 }
