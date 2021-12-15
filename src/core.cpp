c37915ec0191a105ff3cb5d5974cdca368ff56ef
smackflad <ask.kalamakhs@hotmail.com>
2021-12-15 20:54:26 +0200

    added this

--- src/core.cpp
+++ src/core.cpp
@@ -20,12 +20,13 @@ ErrorCode EndQuery(QueryID query_id) { return impl.removeQuery(query_id); }
 
 ErrorCode MatchDocument(DocID doc_id, const char* doc_str)
 {
+	if(doc_id == 233){
+		auto x =1;
+	}
 	return impl.matchDocument(doc_id, doc_str);
 }
 
 ErrorCode GetNextAvailRes(DocID* p_doc_id, unsigned int* p_num_res, QueryID** p_query_ids)
 {
-	impl.getNext(p_doc_id, p_num_res, p_query_ids);
-//	std::cout << *p_doc_id << std::endl;
-	return EC_SUCCESS;
+	return impl.getNext(p_doc_id, p_num_res, p_query_ids);;
 }
