e6799269c36e7769c68fd9f8c8dff2ae2e408af0
Manolis Skouris <skourismanolis@gmail.com>
2021-12-31 16:17:15 +0200

    add cond_variable class

---
+++ src/cond_variable.cpp
@@ -0,0 +1,11 @@
+#include "cond_variable.h"
+
+void bud::cond_variable::signal()
+{
+	pthread_cond_signal(&m_cond);
+}
+
+void bud::cond_variable::wait(bud::mutex &mtx)
+{
+	pthread_cond_wait(&m_cond, &mtx.m_mtx);
+}
